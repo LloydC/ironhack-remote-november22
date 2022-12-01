7d6c6f53eeb47b4a69ca89464fe1a74e50951351
LloydC <lloydchambrier@gmail.com>
2022-12-01 16:03:07 +0100

    chore: canvas-codealong finish

--- module1/lesson-09/canvas-codealong/javascript/index.js
+++ module1/lesson-09/canvas-codealong/javascript/index.js
@@ -1,5 +1,8 @@
+const myObstacles = [];
+
 const myGameArea = {
     canvas: document.createElement('canvas'),
+    frames: 0, // used so we can know when to generate obstacles
     start: function () {
         // setting canvas width and height on the page
       this.canvas.width = 480;
@@ -10,6 +13,15 @@ const myGameArea = {
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      },
+      stop: function () {
+        clearInterval(this.interval);
+      },
+      score: function () {
+        const points = Math.floor(this.frames / 5);
+        this.context.font = '18px serif';
+        this.context.fillStyle = 'black';
+        this.context.fillText(`Score: ${points}`, 350, 50);
       },
   };
 
@@ -24,6 +36,24 @@ const myGameArea = {
       this.speedX = 0;
       this.speedY = 0;
     }
+    left() {
+        return this.x;
+    }
+    
+    right() {
+        return this.x + this.width;
+    }
+    
+    top() {
+        return this.y;
+      }
+      bottom() {
+        return this.y + this.height;
+      }
+
+    crashWith(obstacle) {
+        return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
+    }
    
     newPos() { // updates our component position
         this.x += this.speedX;
@@ -37,10 +67,46 @@ const myGameArea = {
     }
   }
 
+  function updateObstacles(){
+    // define the logic for generating my obstacles on canvas
+    for (i = 0; i < myObstacles.length; i++) {
+        myObstacles[i].x += -1; // decrease x by 1 every 20ms
+        myObstacles[i].update(); // update the obstacles position every 20ms
+    }
+
+    myGameArea.frames += 1; 
+    // every 20ms --> 1 
+    // every 2400ms--> 120
+    if (myGameArea.frames % 120 === 0) { // every 120 frames
+      let x = myGameArea.canvas.width; 
+      let minHeight = 20;
+      let maxHeight = 200;
+      let height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);// randomly generate the height of 
+      let minGap = 50;
+      let maxGap = 200;
+      let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+      myObstacles.push(new Component(10, height, 'green', x, 0)); // adding an obstacle
+      myObstacles.push(new Component(10, x - height - gap, 'green', x, height + gap)); // adding another obstacle
+    }
+  }
+
+  function checkGameOver() {
+    const crashed = myObstacles.some(function (obstacle) {// check if one of the obstacles has crashed with the player
+      return player.crashWith(obstacle);
+    });
+   
+    if (crashed) { // if one of the obstacles has crashed then stop the game
+      myGameArea.stop();
+    }
+  }
+
   function updateGameArea() {
     myGameArea.clear(); // Clear canvas
     player.newPos(); // updates player position
     player.update(); // draws the player on Canvas
+    updateObstacles() // update canvas with obstacles
+    checkGameOver() // stop the game in case of a collision with an obstacle
+    myGameArea.score()// updates game score on canvas
   }
 
   const player = new Component(30, 30, 'red', 0, 110);
