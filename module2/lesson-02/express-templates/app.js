33959fca501ae7d724234f79e6d3a808bc8b9304
LloydC <lloydchambrier@gmail.com>
2022-12-13 15:37:27 +0100

    chore: lesson-02 code update

--- module2/lesson-02/express-templates/app.js
+++ module2/lesson-02/express-templates/app.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const hbs = require('hbs');
 
 const studentList = require('./sampleData/data');
 const port = 3000;
 
 app.set('view engine', 'hbs') // configuring express to use handlebars as a template engine
-app.set('views', 'views') // configures express to look for handlebars files in the 'views' folder
+app.set('views', `${__dirname}/views`) // configures express to look for handlebars files in the 'views' folder
+
+hbs.registerPartials(__dirname + "/views/partials");// this registers my partials
 
 app.use(express.static(path.join(__dirname, 'public'))); // __dirname + 'public'
 
@@ -23,4 +27,32 @@ app.get('/class', (req, res) => {
     res.render('class', { students: studentList })
 })
 
+app.get('/randomCatFacts', (req, res) => {
+// Only works with node version 17.5 or higher 
+
+//   fetch('https://meowfacts.herokuapp.com/')
+//     .then(response => response.json())
+//     .then( data =>{
+//         console.log(data.data[0])
+//         res.render('randomCatFacts', { randomFact: data.data[0] })
+//     })
+
+    axios.get('https://meowfacts.herokuapp.com?count=3')
+    .then(data =>{
+            // console.log(data.data.data)
+            res.render('randomCatFacts', { randomCatFacts: data.data.data })
+    })
+    .catch(err => console.log(err))
+
+})
+
+app.get('/randomDogFacts', (req, res) => {
+    axios.get('http://dog-api.kinduff.com/api/facts?number=5')
+    .then(data =>{
+            // console.log(data.data.facts)
+            res.render('randomDogFacts', {randomDogFacts: data.data.facts})
+    })
+    .catch(err => console.log(err))
+})
 app.listen(port, ()=> console.log(`We are on port ${port}`))
+
